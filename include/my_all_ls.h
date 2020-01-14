f518fbb98c3efb0d5c2fd9e4f31d7257bc042e43
Fayet Mathis <fmathis@localhost.localdomain>
2020-01-14 18:25:33 +0100

    Add all my include for compile my project ls

---
+++ include/my_all_ls.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2019
+** CPE_solostumper_3_2019
+** File description:
+** lol.h
+*/
+
+#ifndef MY_FUNCTION_STR_H
+#define MY_FUNCTION_STR_H
+
+#include <stdlib.h>
+
+#include "my.h"
+
+int my_ls(char *argv);
+
+#endif /* !MY_FUNCTION_STR_H */
