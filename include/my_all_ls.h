5f4238228366474e54721955cadb80a9c199eb19
Fayet Mathis <fmathis@desktop-bi466o6.home>
2020-02-16 18:34:08 +0100

    Add include for add all proto

--- include/my_all_ls.h
+++ include/my_all_ls.h
@@ -9,9 +9,19 @@
 #define MY_FUNCTION_STR_H
 
 #include <stdlib.h>
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+#include <dirent.h>
+#include <unistd.h>
 #include "my.h"
 
-int my_ls(char *argv);
+int my_ls(char **argv);
+int my_ls_basic(char *argv);
+int my_ls_l(char *argv);
+int gettype(struct stat info_file);
+int getlaw(struct stat info_file);
 
 #endif /* !MY_FUNCTION_STR_H */
