e6909b87a8899363a76986b9d9ef60b62738f508
Fayet Mathis <fmathis@desktop-bi466o6.home>
2020-02-16 18:33:36 +0100

    Add function lib for compile better

--- lib/my/my_put_nbr.c
+++ lib/my/my_put_nbr.c
@@ -7,49 +7,16 @@
 
 void my_putchar(char);
 
-int taller(int pdr)
-{
-    int i = 0;
-
-    while (pdr != 0) {
-        pdr = pdr / 10;
-        i++;
-    }
-    return (i);
-}
-
-int counter(int x, int y)
-{
-    int count = y;
-    int sell = 1;
-
-    while (x > sell) {
-        y = y * count;
-        sell++;
-    }
-    return (y);
-}
-
-int activate(int tall, int modulo)
-{
-    while (tall > 0) {
-        my_putchar(modulo * 10 / counter(tall, 10) + '0');
-        if (tall != 0)
-            modulo = modulo % counter(tall - 1, 10);
-        tall--;
-    }
-    return (modulo);
-}
-
 int my_put_nbr(int nb)
 {
-    int modulo = nb;
-    int tall = taller(nb);
-
     if (nb < 0) {
-        modulo = nb * (-1);
-        tall = taller(modulo);
         my_putchar('-');
+        if (nb <= -10)
+            my_put_nbr((nb/10) * -1);
+        my_putchar(((nb % 10) * - 1) + 48);
+    } else {
+        if (nb >= 10)
+            my_put_nbr(nb / 10);
+        my_putchar((nb % 10) + 48);
     }
-    return (modulo);
 }
--- lib/my/my_str_to_word_array.c
+++ lib/my/my_str_to_word_array.c
@@ -5,7 +5,71 @@
 ** LIBRAIRIE
 */
 
-char **my_str_to_word_array(char const *str)
+#include <stdlib.h>
+
+int length(char *str)
 {
-    return (0);
+    int first = 0;
+    int count  = 0;
+
+    while (str[first] != '\0') {
+        if (str[first] == ' ')
+            count += 1;
+        first += 1;
+    }
+    return (count + 2);
+}
+
+char **my_malloc(char **str, int length, int len)
+{
+    int index = 0;
+
+    while (index < length) {
+        str[index] = malloc(sizeof(char) * len);
+        index += 1;
+    }
+    return (str);
+}
+
+int length_my_str(char *str)
+{
+    int index = 0;
+
+    while (str[index] != '\0') {
+        index += 1;
+    }
+    return (index);
+}
+
+char **copy_my_str(char *str, char **dest)
+{
+    int index = 0;
+    int x = 0;
+    int y = 0;
+
+    while (str[index] != '\0') {
+        if (str[index] == ':') {
+            dest[x][y] = '\0';
+            x += 1;
+            y = 0;
+            index += 1;
+        }
+        dest[x][y] = str[index];
+        y += 1;
+        index += 1;
+    }
+    dest[x + 1] = NULL;
+    return (dest);
+}
+
+char **my_str_to_word_array(char *str)
+{
+    int index = 0;
+    int result = length(str);
+    char **dest = malloc(sizeof(char *) * result);
+    int len = length_my_str(str);
+
+    dest = my_malloc(dest, result, len);
+    dest = copy_my_str(str, dest);
+    return (dest);
 }
